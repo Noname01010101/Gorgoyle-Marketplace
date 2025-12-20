3d5e7bd161f7ad1682a0a0772c039bac0efbef6d
Túlio <kmaopop@gmail.com>
2025-12-20 15:02:36 -0300

    docs(hooks): improve commit message validation error guide

--- api/tests/test.test.ts
+++ api/tests/test.test.ts
@@ -1,5 +1,5 @@
-describe("Sample Test Suite", () => {
-  it("Sample Test Case", () => {
+describe('Sample Test Suite', () => {
+  it('Sample Test Case', () => {
     expect(1 + 1).toBe(2);
-  }); 
+  });
 });
